4a061c1bd23576b6a5a3e1107122b333f5d6c3ea
Saideepika <912183@cognizant.com>
2023-10-20 11:50:22 +0530

    stages

---
+++ env-prod/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-a21"
+key = "roboshop/prod/terraform.tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,7 +1,3 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform-a21"
-        key = "roboshop/dev/terraform.tfstate"
-        region = "us-east-1"
-    }
+    backend "s3" {}
 }
